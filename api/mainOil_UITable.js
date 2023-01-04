b7bcb1468cdf9df978b3b44ba65afb0a9b01bd6f
95度茅台 <dj4qiao@sina.com>
2023-01-05 05:24:11 +0800

    update script

--- api/mainOil_UITable.js
+++ api/mainOil_UITable.js
@@ -222,12 +222,9 @@ async function main() {
           name: 'fuelpump.fill',
           color: '#43CD80'
         },
-        type: 'web',
+        type: 'html',
         title: '调整日期',
-        val: '>',
-        onClick: async () => {
-          Safari.openInApp('https://gitcode.net/4qiao/framework/raw/master/img/picture/oilAdjustmentDate.jpeg', false);
-        }
+        val: '>'
       },
       {
         icon: {
@@ -469,6 +466,8 @@ async function main() {
         } else if (type == 'preview') {
           let importedModule = importModule(modulePath);
           await importedModule.main();
+        } else if (type == 'html') {
+          await adjustmentDate();
         }
       }
     }
@@ -657,6 +656,216 @@ async function main() {
       notify('小组件更新成功', '');
       Safari.open('scriptable:///run/' + encodeURIComponent(uri));
     }
+  }
+  
+  
+  /**
+   * Oil Price Adjustment Date
+   * @param { HTML } string
+   */
+  async function adjustmentDate() {
+      const webView = new WebView()
+    const style = `
+      <meta charset="utf-8" >
+        <title>全国油价调整日期</title>
+      <meta name="viewport" content="width=device-width,initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+        <style type="text/css">
+       .box{
+        width=100%;
+        border-radius: 10px;
+        padding-top: 12px;
+        padding-bottom: 12px;
+        margin-bottom: 12px;
+        margin-top: 12px;
+       }
+       .words{
+        text-align:justify;
+        line-height: 20px;
+        margin: 0px auto;
+        width: 90%;
+       }
+        </style>`
+    const webHtml = style + `
+    <body bgcolor=white>
+     <h3>
+      <center>
+       <font color=black>
+        2023 年 25 次油价调整
+       </font>
+      </center>
+     </h3>
+     </div>
+     </div>
+     </div>
+     <div class="box"
+       style="background-color:#7DD1F0;">
+      <div class="words">
+       <span style="color: #101418;">
+        <b> 1月油价调整时间 </b>
+       </span>
+       <span style="color: #101418; float: right;">
+        <b>1月3日24时、1月17日24时</b>
+       </span>
+      </div>
+     </div>
+     </div>
+    
+     <div class="box"
+       style="background-color:#FFA0A3;">
+      <div class="words">
+       <span style="color: #101418;">
+        <b> 2月油价调整时间 </b>
+       </span>
+       <span style="color: #101418; float: right;">
+        <b>2月3日24时、2月17日24时</b>
+       </span>
+      </div>
+     </div>
+     </div>
+    
+     <div class="box"
+       style="background-color:#FFE669;">
+      <div class="words">
+       <span style="color: #101418;">
+        <b> 3月油价调整时间 </b>
+       </span>
+       <span style="color: #101418; float: right;">
+        <b>3日24时、17日24时、31日24时</b>
+       </span>
+      </div>
+     </div>
+     </div>
+    
+     <div class="box"
+       style="background-color:#7DE6C2;">
+      <div class="words">
+       <span style="color: #101418;">
+        <b> 4月油价调整时间 </b>
+       </span>
+       <span style="color: #101418; float: right;">
+        <b>4月17日24时、4月28日24时</b>
+       </span>
+      </div>
+     </div>
+     </div>
+    
+     <div class="box"
+       style="background-color:#E0B0FF;">
+      <div class="words">
+       <span style="color: #101418;">
+        <b> 5月油价调整时间 </b>
+       </span>
+       <span style="color: #101418; float: right;">
+        <b>5月16日24时、5月30日24时</b>
+       </span>
+      </div>
+     </div>
+     </div>
+    
+     <div class="box"
+       style="background-color:#FBCEB1;">
+      <div class="words">
+       <span style="color: #101418;">
+        <b> 6月油价调整时间 </b>
+       </span>
+       <span style="color: #101418; float: right;">
+        <b>6月13日24时、6月28日24时</b>
+       </span>
+      </div>
+     </div>
+     </div>
+    
+     <div class="box"
+       style="background-color:#FFE669;">
+      <div class="words">
+       <span style="color: #101418;">
+        <b> 7月油价调整时间 </b>
+       </span>
+       <span style="color: #101418; float: right;">
+        <b>7月12日24时、7月26日24时</b>
+       </span>
+      </div>
+     </div>
+     </div>
+    
+     <div class="box"
+       style="background-color:#7DE6C2;">
+      <div class="words">
+       <span style="color: #101418;">
+        <b> 8月油价调整时间 </b>
+       </span>
+       <span style="color: #101418; float: right;">
+        <b>8月9日24时、8月23日24时</b>
+       </span>
+      </div>
+     </div>
+     </div>
+    
+     <div class="box"
+       style="background-color:#7DD1F0;">
+      <div class="words">
+       <span style="color: #101418;">
+        <b> 9月油价调整时间 </b>
+       </span>
+       <span style="color: #101418; float: right;">
+        <b>9月6日24时、9月20日24时</b>
+       </span>
+      </div>
+     </div>
+     </div>
+    
+     <div class="box"
+       style="background-color:#FFA0A3;">
+      <div class="words">
+       <span style="color: #101418;">
+        <b> 10月油价调整时间 </b>
+       </span>
+       <span style="color: #101418; float: right;">
+        <b>10月10日24时、10月24日24时</b>
+       </span>
+      </div>
+     </div>
+     </div>
+    
+     <div class="box"
+       style="background-color:#FAC898;">
+      <div class="words">
+       <span style="color: #101418;">
+        <b> 11月油价调整时间 </b>
+       </span>
+       <span style="color: #101418; float: right;">
+        <b>11月7日24时、11月21日24时</b>
+       </span>
+      </div>
+     </div>
+     </div>
+    
+     <div class="box"
+       style="background-color:#E0B0FF;">
+      <div class="words">
+       <span style="color: #101418;">
+        <b> 12月油价调整时间 </b>
+       </span>
+       <span style="color: #101418; float: right;">
+        <b>12月5日24时、12月19日24时</b>
+       </span>
+      </div>
+     </div>
+     </div>
+    
+     <div class="boxx"
+       style="background-color:white;width=100%;
+    text-align:center;
+    padding-left: 5px;
+    padding-right: 5px;  
+    <div  class="
+       words">
+     </div>
+     </div>
+    </body>
+    `
+    await webView.loadHTML(webHtml)
+    await webView.present()
   }
   
   
