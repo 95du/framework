1ce30d773467b7d85ced3b7355471da3dd5e4133
95度茅台 <dj4qiao@sina.com>
2023-01-10 18:36:06 +0800

    Update text

--- scriptable/colorFinder.js
+++ scriptable/colorFinder.js
@@ -12,9 +12,7 @@
 <header class="flex items-center">
       <div class="header__logo mr05"></div>
       <button class="mdc-icon-button" data-btn-info type="button" aria-label="project info" style="display:none;">
-        <svg class="mdc-icon-button__icon" width="18" height="18" viewBox="0 0 24 24" aria-hidden="true">
-          <path d="M11,9H13V7H11M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M11,17H13V11H11V17Z"/>
-        </svg>
+        
       </button>
       <div class="header__input-wrapper flex flex-auto">
         <div class="flex items-center">
