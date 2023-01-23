4b6a414ea30df45254eb1b5568b8cd606e152827
95度茅台 <dj4qiao@sina.com>
2023-01-24 05:41:53 +0800

    update script

--- api/main12123_UITable.js
+++ api/main12123_UITable.js
@@ -191,7 +191,7 @@ async function main() {
                 color: '#43CD80'
               },
               type: 'OS',
-              title: iOS ? iOS.match(/(iOS\s\d+\.\d*?\.?\d*?\s(beta\s?[\d*]?|RC\s?\d?))/)[1] : '正式版已发布',
+              title: iOS.indexOf('RC' || 'beta') !== -1 ? iOS.match(/(iOS\s\d+\.\d*?\.?\d*?\s(beta\s?[\d*]?|RC\s?\d?))/)[1] : iOS,
               val: iOS ? iOS.match(/\((.*?)\)/)[1] : '>',
               ios: iOS
             },
@@ -202,7 +202,7 @@ async function main() {
               },
               type: 'OS',
               title: html.match(/<title>(iOS\s\d+\.\d\.?\d?)\s\(/)[1],
-              val: '>',
+              val: '>'
             },
             {
               icon: {
@@ -211,7 +211,7 @@ async function main() {
               },
               type: 'OS',
               title: html.match(/<title>(iOS\s15\.\d\.?\d?)\s\(/)[1],
-              val: '>',
+              val: '>'
             },
             {
               interval: 26
@@ -222,8 +222,8 @@ async function main() {
                 color: '#F9A825'
               },
               type: 'OS',
-              title: iPadOS ? iPadOS.match(/(iPadOS\s\d+\.\d*?\.?\d*?\s(beta\s?[\d*]?|RC\s?\d?))/)[1] : '正式版已发布',
-              val: iPadOS ? iPadOS.match(/\((.*?)\)/)[1] : '>',
+              title: iPadOS.indexOf('RC' || 'beta') !== -1 ? iOS.match(/(iOS\s\d+\.\d*?\.?\d*?\s(beta\s?[\d*]?|RC\s?\d?))/)[1] : iPadOS,
+              val: iPadOS ? iPadOS.match(/\((.*?)\)/)[1] : '>'
             },
             {
               icon: {
@@ -232,7 +232,7 @@ async function main() {
               },
               type: 'OS',
               title: html.match(/<title>(iPadOS\s\d+\.\d\.?\d?)\s\(/)[1],
-              val: '>',
+              val: '>'
             },
             {
               icon: {
@@ -241,7 +241,7 @@ async function main() {
               },
               type: 'OS',
               title: html.match(/<title>(iPadOS\s15\.\d\.?\d?)\s\(/)[1],
-              val: '>',
+              val: '>'
             },
             {
               interval: 130.5 * Device.screenScale()
@@ -291,7 +291,7 @@ async function main() {
               type: 'input',
               title: '刷新时间',
               desc: '尝试改变刷新组件时间\n具体时间由系统判断，单位: 分钟',
-              val: 'minute',
+              val: 'minute'
             },
             {
               url: 'https://gitcode.net/4qiao/framework/raw/master/img/symbol/carPicture.png',
