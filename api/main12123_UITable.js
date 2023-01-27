3f2c7f8164860ad4d4a14584ab86f9a9a7bec202
95度茅台 <dj4qiao@sina.com>
2023-01-27 17:45:45 +0800

    update script

--- api/main12123_UITable.js
+++ api/main12123_UITable.js
@@ -116,8 +116,8 @@ async function main() {
     // Header effectImage Row
     const effectRow = new UITableRow();
     effectRow.height = 70 * Device.screenScale();
-    const topImg = ['aHR0cDovL210dy5zby81UkoyQlM=', 'aHR0cDovL210dy5zby82Nkw0Q0U=', 
-  'aHR0cDovL210dy5zby82ZjFVZjQ=']
+    const topImg = ['aHR0cDovL210dy5zby81dm9Jc0Y=', 'aHR0cDovL210dy5zby81dm9JdHQ=', 
+  'aHR0cDovL210dy5zby82NzBldmY=']
     const items = topImg[Math.floor(Math.random()*topImg.length)];
     const effectImage = effectRow.addImageAtURL(atob(items));
     effectImage.widthWeight = 0.4;
