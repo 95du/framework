1f7fca6eefe075a224ec4e18b77e4709bbcfe731
95度茅台 <dj4qiao@sina.com>
2023-02-09 01:04:52 +0800

    Update text

--- scriptable/adjustmentDate.js
+++ scriptable/adjustmentDate.js
@@ -1,4 +1,3 @@
-
   <meta charset="utf-8" >
     <title>全国油价调整日期</title>
   <meta name="viewport" content="width=device-width,initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
@@ -17,8 +16,8 @@
     margin: 0px auto;
     width: 90%;
    }
-    </style>`
-
+    </style>
+    
 <body bgcolor=white>
  <h3>
   <center>
