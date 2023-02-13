cf92e4c784c1d5c13f80b960d069fddb7d8ae4b9
95度茅台 <dj4qiao@sina.com>
2023-02-14 02:50:55 +0800

    update script

--- api/mainTelecom_UITable.js
+++ api/mainTelecom_UITable.js
@@ -602,10 +602,9 @@ async function main() {
             await generateInputAlert ({
               title: title,
               message: (val === 'gradient') ? desc + setting[val] : desc,
-              options: [{ 
-                hint: !tips ? setting[val] : tips,
-                value: !tips ? setting[val] : null
-              }]
+              options: [
+                { hint: !tips ? setting[val] : tips, value: !tips ? setting[val] : null }
+              ]
             }, 
             async (inputArr) => {
               const filedVal = inputArr[0].value;
