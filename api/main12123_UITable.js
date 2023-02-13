51b16080b5119caabf50c88ea55c0a95157c9f1f
95度茅台 <dj4qiao@sina.com>
2023-02-14 02:17:59 +0800

    update script

--- api/main12123_UITable.js
+++ api/main12123_UITable.js
@@ -359,7 +359,7 @@ async function main() {
               url: 'https://gitcode.net/4qiao/framework/raw/master/img/symbol/photoSize.png',
               type: 'input',
               title: '图下字符',
-              desc: '设置在19个字符以内',
+              desc: '保持只显示一行字符',
               val: 'botStr',
               str: '输入字符'
             },
