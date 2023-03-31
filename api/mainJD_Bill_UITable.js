29dea83c0aa34c6a0e44e8c30c2daf45d6b2399b
95度茅台 <dj4qiao@sina.com>
2023-03-31 22:35:39 +0800

    update script

--- api/mainJD_Bill_UITable.js
+++ api/mainJD_Bill_UITable.js
@@ -188,7 +188,7 @@ async function main() {
           const iPadOS = html.match(/<title>(iPadOS.*?)<\/title>/)[1];
           const arr = html.split('<item>');
           
-          let newArr =[];
+          let newArr = [];
           for (const item of arr) {
             const iOS = item.match(/<title>(.*?)<\/title>/)[1];
             if (iOS.indexOf('iOS 16') > -1) {
