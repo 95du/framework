a0e573688670016866d1baec92d613febec51869
95度茅台 <dj4qiao@sina.com>
2023-04-06 03:45:37 +0800

    update script

--- scriptable/webSettings.js
+++ scriptable/webSettings.js
@@ -1,6 +1,7 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: purple; icon-glyph: cog;
+
 async function main() {
   const F_MGR = FileManager.local();
   const path = F_MGR.joinPath(F_MGR.documentsDirectory(), "95du_electric");
