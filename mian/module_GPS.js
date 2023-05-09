57e78909446a1bb12723b38185baf7dfabe16250
95度茅台 <dj4qiao@sina.com>
2023-05-09 14:37:39 +0800

    upload new script

---
+++ mian/module_GPS.js
@@ -0,0 +1,80 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: blue; icon-glyph: car;
+/**
+ * 脚本名称: 车辆GPS定位
+ * 小组件作者：95度茅台
+ * 组件功能: 显示车辆实时位置、车速、最高时速、行车里程和停车时间等。推送实时静态地图及信息到微信。
+ * 需申请高德地图key，微信推送需要另外填入企业微信应用的链接。
+ * 版本: Version 1.0.0
+ * 2023-05-01 15:00
+ * 🔥 商务QQ: 352388731
+*/
+
+const scriptName = '95duGps';
+const scriptUrl = atob('aHR0cHM6Ly9naXRjb2RlLm5ldC80cWlhby9mcmFtZXdvcmsvcmF3L21hc3Rlci9hcGkvbWFpbl9HUFMuanM=');
+const fm = FileManager.local();
+const runPath = fm.joinPath(fm.documentsDirectory(), scriptName);
+if (!fm.fileExists(runPath)) {
+  fm.createDirectory(runPath);
+}
+
+const moduleDir = fm.joinPath(fm.documentsDirectory(), `${scriptName}/Running`);
+if (!fm.fileExists(moduleDir)) {
+  fm.createDirectory(moduleDir);
+}
+
+const modulePath = await downloadModule(scriptName, scriptUrl);
+if (modulePath != null) {
+  const importedModule = importModule(modulePath);
+  await importedModule.main();
+}
+
+async function downloadModule(scriptName, scriptUrl) {
+  const date = new Date();
+  const df = new DateFormatter();
+  df.dateFormat = 'yyyyMMddHH';
+  const moduleFilename = df.string(date).toString() + '.js';
+  const modulePath = fm.joinPath(moduleDir, moduleFilename);
+  if (fm.fileExists(modulePath)) {
+    return modulePath;
+  } else {
+    const [moduleFiles, moduleLatestFile] = getModuleVersions(scriptName);
+    const req = new Request(scriptUrl);
+    const moduleJs = await req.load().catch(() => {
+      return null;
+    });
+    if (moduleJs) {
+      fm.write(modulePath, moduleJs);
+      if (moduleFiles != null) {
+        moduleFiles.map(x => {
+          fm.remove(fm.joinPath(moduleDir, x));
+        });
+      }
+      return modulePath;
+    } else {
+      console.log('Failed to download new module. Using latest local version: ' + moduleLatestFile);
+      return (moduleLatestFile != null) ? fm.joinPath(moduleDir, moduleLatestFile) : null;
+    }
+  }
+}
+
+function getModuleVersions(scriptName) {
+  const dirContents = fm.listContents(moduleDir);
+  if (dirContents.length > 0) {
+    const versions = dirContents.map(x => {
+      if (x.endsWith('.js')) return parseInt(x.replace('.js', ''));
+    });
+    versions.sort(function(a, b) {
+      return b - a;
+    });
+    if (versions.length > 0) {
+      const moduleFiles = versions.map(x => {
+        return x + '.js';
+      });
+      moduleLatestFile = versions[0] + '.js';
+      return [moduleFiles, moduleLatestFile];
+    }
+  }
+  return [null, null];
+}
