44df56fd007dacd3bf8ff6d01b1f425bdf5c7915
95度茅台 <dj4qiao@sina.com>
2023-07-08 08:36:43 +0800

    upload new script

---
+++ mian/module_95du_storeScript.js
@@ -0,0 +1,80 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: pink; icon-glyph: cloud-download-alt;
+/**
+ * 组件名称: 小组件商店
+ * 小组件作者: 95度茅台
+ * Version 1.0.0
+ * 2023-07-07
+ */
+  
+async function main() {
+  const scriptName = '95du_store';
+  const scriptUrl = atob('aHR0cHM6Ly9naXRjb2RlLm5ldC80cWlhby9mcmFtZXdvcmsvcmF3L21hc3Rlci9hcGkvbWFpbl85NWR1U2NyaXB0Lmpz');
+  const fm = FileManager.local();
+  const runPath = fm.joinPath(fm.documentsDirectory(), scriptName);
+  if (!fm.fileExists(runPath)) {
+    fm.createDirectory(runPath);
+  }
+  
+  const moduleDir = fm.joinPath(fm.documentsDirectory(), `${scriptName}/Running`);
+  if (!fm.fileExists(moduleDir)) {
+    fm.createDirectory(moduleDir);
+  }
+  
+  const modulePath = await downloadModule(scriptName, scriptUrl);
+  if (modulePath != null) {
+    const importedModule = importModule(modulePath);
+    importedModule.main();
+  }
+  
+  async function downloadModule(scriptName, scriptUrl) {
+    const date = new Date();
+    const df = new DateFormatter();
+    df.dateFormat = 'yyyyMMddHH';
+    const moduleFilename = df.string(date).toString() + '.js';
+    const modulePath = fm.joinPath(moduleDir, moduleFilename);
+    if (fm.fileExists(modulePath)) {
+      return modulePath;
+    } else {
+      const [moduleFiles, moduleLatestFile] = getModuleVersions(scriptName);
+      const req = new Request(scriptUrl);
+      const moduleJs = await req.load().catch(() => {
+        return null;
+      });
+      if (moduleJs) {
+        fm.write(modulePath, moduleJs);
+        if (moduleFiles != null) {
+          moduleFiles.map(x => {
+            fm.remove(fm.joinPath(moduleDir, x));
+          });
+        }
+        return modulePath;
+      } else {
+        console.log('Failed to download new module. Using latest local version: ' + moduleLatestFile);
+        return (moduleLatestFile != null) ? fm.joinPath(moduleDir, moduleLatestFile) : null;
+      }
+    }
+  }
+  
+  function getModuleVersions(scriptName) {
+    const dirContents = fm.listContents(moduleDir);
+    if (dirContents.length > 0) {
+      const versions = dirContents.map(x => {
+        if (x.endsWith('.js')) return parseInt(x.replace('.js', ''));
+      });
+      versions.sort(function(a, b) {
+        return b - a;
+      });
+      if (versions.length > 0) {
+        const moduleFiles = versions.map(x => {
+          return x + '.js';
+        });
+        moduleLatestFile = versions[0] + '.js';
+        return [moduleFiles, moduleLatestFile];
+      }
+    }
+    return [null, null];
+  }
+}
+module.exports = { main }
