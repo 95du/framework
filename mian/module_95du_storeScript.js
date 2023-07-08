dc6c98cfda2b4f84498c3106ddd8ef7ebe6c93cb
95度茅台 <dj4qiao@sina.com>
2023-07-08 15:06:37 +0800

    update script

--- mian/module_95du_storeScript.js
+++ mian/module_95du_storeScript.js
@@ -7,7 +7,7 @@
  * Version 1.0.0
  * 2023-07-07
  */
-main()
+
 async function main() {
   const scriptName = '95du_store';
   const scriptUrl = atob('aHR0cHM6Ly9naXRjb2RlLm5ldC80cWlhby9zY3JpcHRhYmxlLy0vcmF3L21hc3Rlci92aXAvbWFpbjk1ZHVTdG9yZS5qcw==');
