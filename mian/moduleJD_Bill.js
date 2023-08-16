f881c4000fa0899f6e92480d1b543da27fda9b5f
95度茅台 <dj4qiao@sina.com>
2023-08-17 04:38:54 +0800

    update script

--- mian/moduleJD_Bill.js
+++ mian/moduleJD_Bill.js
@@ -6,75 +6,65 @@
  * 小组件作者：95度茅台
  * 版本: Version 1.0.2
  * 2023-03-13 15:00
- * Telegram 交流群 https://t.me/+ViT7uEUrIUV0B_iy
+ * Telegram 交流群 https://t.me/+CpAbO_q_SGo2ZWE1
  * 🔥示例图渐变颜色 #FFE5B4 
  * LSP京东组件的背景图 https://gitcode.net/enoyee/scriptable/-/raw/master/img/jd/bg_orange.png
 */
 
 const scriptName = '95duJingDong_Bill';
 const scriptUrl = atob('aHR0cHM6Ly9naXRjb2RlLm5ldC80cWlhby9mcmFtZXdvcmsvcmF3L21hc3Rlci9hcGkvbWFpbkpEX0JpbGxfVUlUYWJsZS5qcw==');
+
 const fm = FileManager.local();
 const runPath = fm.joinPath(fm.documentsDirectory(), scriptName);
-if (!fm.fileExists(runPath)) {
-  fm.createDirectory(runPath);
-}
+const moduleDir = fm.joinPath(runPath, 'Running');
 
-const moduleDir = fm.joinPath(fm.documentsDirectory(), `${scriptName}/Running`);
-if (!fm.fileExists(moduleDir)) {
-  fm.createDirectory(moduleDir);
-}
+if (!fm.fileExists(runPath)) fm.createDirectory(runPath);
+if (!fm.fileExists(moduleDir)) fm.createDirectory(moduleDir);
 
-const modulePath = await downloadModule(scriptName, scriptUrl);
-if (modulePath != null) {
-  const importedModule = importModule(modulePath);
-  await importedModule.main();
-}
-
-async function downloadModule(scriptName, scriptUrl) {
+const downloadModule = async () => {
   const date = new Date();
   const df = new DateFormatter();
   df.dateFormat = 'yyyyMMddHH';
+  
   const moduleFilename = df.string(date).toString() + '.js';
   const modulePath = fm.joinPath(moduleDir, moduleFilename);
-  if (fm.fileExists(modulePath)) {
-    return modulePath;
-  } else {
-    const [moduleFiles, moduleLatestFile] = getModuleVersions(scriptName);
+
+  if (fm.fileExists(modulePath)) return modulePath;
+
+  const [moduleFiles, moduleLatestFile] = getModuleVersions();
+
+  try {
     const req = new Request(scriptUrl);
-    const moduleJs = await req.load().catch(() => {
-      return null;
-    });
+    const moduleJs = await req.load();
     if (moduleJs) {
       fm.write(modulePath, moduleJs);
-      if (moduleFiles != null) {
-        moduleFiles.map(x => {
-          fm.remove(fm.joinPath(moduleDir, x));
-        });
-      }
+      if (moduleFiles) moduleFiles.forEach(file => fm.remove(fm.joinPath(moduleDir, file)));
       return modulePath;
     } else {
-      console.log('Failed to download new module. Using latest local version: ' + moduleLatestFile);
-      return (moduleLatestFile != null) ? fm.joinPath(moduleDir, moduleLatestFile) : null;
+      return moduleLatestFile ? fm.joinPath(moduleDir, moduleLatestFile) : null;
     }
+  } catch (e) {
+    return moduleLatestFile ? fm.joinPath(moduleDir, moduleLatestFile) : null;
   }
-}
+};
 
-function getModuleVersions(scriptName) {
+const getModuleVersions = () => {
   const dirContents = fm.listContents(moduleDir);
   if (dirContents.length > 0) {
-    const versions = dirContents.map(x => {
-      if (x.endsWith('.js')) return parseInt(x.replace('.js', ''));
-    });
-    versions.sort(function(a, b) {
-      return b - a;
-    });
+    const versions = dirContents.map(x => parseInt(x.replace('.js', '')));
+    versions.sort((a, b) => b - a);
+
     if (versions.length > 0) {
-      const moduleFiles = versions.map(x => {
-        return x + '.js';
-      });
-      moduleLatestFile = versions[0] + '.js';
+      const moduleFiles = versions.map(x => `${x}.js`);
+      const moduleLatestFile = `${versions[0]}.js`;
       return [moduleFiles, moduleLatestFile];
     }
   }
   return [null, null];
-}
+};
+
+const modulePath = await downloadModule();
+if (modulePath) {
+  const importedModule = await importModule(modulePath);
+  await importedModule.main();
+};
