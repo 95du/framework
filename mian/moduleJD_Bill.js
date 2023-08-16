839ec0e6c8bed2def1594c718bbe92743a4217d7
95度茅台 <dj4qiao@sina.com>
2023-08-17 04:53:42 +0800

    update script

--- mian/moduleJD_Bill.js
+++ mian/moduleJD_Bill.js
@@ -66,5 +66,5 @@ const getModuleVersions = () => {
 const modulePath = await downloadModule();
 if (modulePath) {
   const importedModule = await importModule(modulePath);
-  await importedModule.main();
+  importedModule.main();
 };
