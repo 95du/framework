0775735663657b8ecf2a6a5bf37d5ce5f7076434
95度茅台 <dj4qiao@sina.com>
2023-10-07 14:50:54 +0800

    update script

--- api/web_main_12123.js
+++ api/web_main_12123.js
@@ -1328,12 +1328,12 @@ async function main() {
           ScriptableRun();
         }
       } else if ( code === 'recover' ) {
-        const index = await generateAlert(  
+        const action = await generateAlert(  
           '是否恢复设置 ？', 
           '用户登录的信息将重置\n设置的数据将会恢复为默认',   
           options = ['取消', '恢复']
         );
-        if ( index === 1 ) {
+        if ( action === 1 ) {
           writeSettings(DEFAULT);
           ScriptableRun();
         }
@@ -1606,7 +1606,7 @@ async function main() {
             }
           },
           {
-            label: '精选渐变',
+            label: '内置渐变',
             name: 'gradient',
             type: 'select',
             multiple: true,
