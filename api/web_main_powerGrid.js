ebbe4b3699595d58281f3ae4d25ec13dd67df742
95度茅台 <dj4qiao@sina.com>
2023-10-17 21:28:45 +0800

    update script

--- api/web_main_powerGrid.js
+++ api/web_main_powerGrid.js
@@ -1,7 +1,7 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: pink; icon-glyph: bolt;
-await 
+
 async function main() {
   const scriptName = '南方电网'
   const version = '1.0.0'
