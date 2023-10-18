e2ce91cb2ec461eeb8ed3d09da26f43bacd60d9f
95度茅台 <dj4qiao@sina.com>
2023-10-18 22:28:08 +0800

    Replace main_ jingDong_Bean.js

--- api/main_ jingDong_Bean.js
+++ api/main_ jingDong_Bean.js
@@ -1,7 +1,7 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: deep-purple; icon-glyph: tags;
-await 
+main()
 async function main() {
   const uri = Script.name();
   const F_MGR = FileManager.local();
