4c39563d75ef9eb03ea7760e13d624c666403853
95度茅台 <dj4qiao@sina.com>
2023-10-19 06:24:25 +0800

    update script

--- mian/web_module_jingDong_baiTiao.js
+++ mian/web_module_jingDong_baiTiao.js
@@ -9,7 +9,7 @@
  * Telegram 交流群 https://t.me/+CpAbO_q_SGo2ZWE1
 */
 
-const scriptName = '95duJingDong_BaiTiao';
+const scriptName = '95du_jd_baiTiao';
 const scriptUrl = atob('aHR0cHM6Ly9naXRjb2RlLm5ldC80cWlhby9mcmFtZXdvcmsvcmF3L21hc3Rlci9hcGkvd2ViX21haW5famluZ0RvbmdfYmFpVGlhby5qcw==');
 
 const fm = FileManager.local();
