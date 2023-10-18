fb45f40a6cc8dad111100d75aaf9d96bbc045b8d
95度茅台 <dj4qiao@sina.com>
2023-10-19 05:38:12 +0800

    upload new script

---
+++ mian/module_jingDong_bill.js
@@ -0,0 +1,68 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: red; icon-glyph: tags;
+/**
+ * 组件名称: JD_收支账单(月)
+ * 组件作者：95度茅台
+ * 组件版本: Version 1.0.0
+ * 更新日期: 2023-10-18 15:00
+ * Telegram 交流群 https://t.me/+CpAbO_q_SGo2ZWE1
+*/
+
+const scriptName = '95duJingDong_Bill';
+const scriptUrl = atob('aHR0cHM6Ly9naXRjb2RlLm5ldC80cWlhby9mcmFtZXdvcmsvcmF3L21hc3Rlci9hcGkvbWFpbl9qaW5nRG9uZ19CaWxsLmpz');
+
+const fm = FileManager.local();
+const runPath = fm.joinPath(fm.documentsDirectory(), scriptName);
+const moduleDir = fm.joinPath(runPath, 'Running');
+
+if (!fm.fileExists(runPath)) fm.createDirectory(runPath);
+if (!fm.fileExists(moduleDir)) fm.createDirectory(moduleDir);
+
+const downloadModule = async () => {
+  const date = new Date();
+  const df = new DateFormatter();
+  df.dateFormat = 'yyyyMMddHH';
+  
+  const moduleFilename = df.string(date).toString() + '.js';
+  const modulePath = fm.joinPath(moduleDir, moduleFilename);
+
+  if (fm.fileExists(modulePath)) return modulePath;
+
+  const [moduleFiles, moduleLatestFile] = getModuleVersions();
+
+  try {
+    const req = new Request(scriptUrl);
+    const moduleJs = await req.load();
+    if (moduleJs) {
+      fm.write(modulePath, moduleJs);
+      if (moduleFiles) moduleFiles.forEach(file => fm.remove(fm.joinPath(moduleDir, file)));
+      return modulePath;
+    } else {
+      return moduleLatestFile ? fm.joinPath(moduleDir, moduleLatestFile) : null;
+    }
+  } catch (e) {
+    return moduleLatestFile ? fm.joinPath(moduleDir, moduleLatestFile) : null;
+  }
+};
+
+const getModuleVersions = () => {
+  const dirContents = fm.listContents(moduleDir);
+  if (dirContents.length > 0) {
+    const versions = dirContents.map(x => parseInt(x.replace('.js', '')));
+    versions.sort((a, b) => b - a);
+
+    if (versions.length > 0) {
+      const moduleFiles = versions.map(x => `${x}.js`);
+      const moduleLatestFile = `${versions[0]}.js`;
+      return [moduleFiles, moduleLatestFile];
+    }
+  }
+  return [null, null];
+};
+
+const modulePath = await downloadModule();
+if (modulePath) {
+  const importedModule = await importModule(modulePath);
+  importedModule.main();
+};
