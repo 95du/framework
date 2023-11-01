94ed8f1165ff2ba3585b716853cbac7f85a1856c
95度茅台 <dj4qiao@sina.com>
2023-11-02 05:02:33 +0800

    update script

--- api/web_main_china_telecom.js
+++ api/web_main_china_telecom.js
@@ -496,9 +496,7 @@ async function main() {
       previewImage
     } = options;
 
-    // themeColor
-    const [themeColor, logoColor] = Device.isUsingDarkAppearance() ? ['dark-theme', 'white'] : ['white-theme', 'black'];
-
+    const logoColor = Device.isUsingDarkAppearance() ? 'white' : 'black';
     const appleHub = await getCacheImage(`${logoColor}.png`, `${rootUrl}img/picture/appleHub_${logoColor}.png`);
 
     const appImage = await getCacheImage('telecom_5.png', `${rootUrl}img/icon/telecom_1.png`);
@@ -730,7 +728,7 @@ async function main() {
       return `
       <div class="popup-mask" onclick="switchDrawerMenu()"></div>
       <div class="popup-container">
-        <div class="popup-widget blur-bg" role="dialog">
+        <div class="popup-widget zib-widget blur-bg" role="dialog">
           <div class="box-body">
             <img id="app" onclick="switchDrawerMenu()" class="app-icon" src="${appImage}">
             <div class="app-desc">中国电信天翼账号中心</div>
@@ -1142,7 +1140,7 @@ async function main() {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
       <style>${style}</style>
       </head>
-      <body class="${themeColor}">
+      <body>
         ${avatarInfo ? await mainMenuTop() : previewImage ? (settings.clock ? clockScript : await previewImgHtml()) : ''}
         <!-- 弹窗 -->
         ${previewImage ? await donatePopup() : ''}
