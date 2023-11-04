5f577704dd0f55046b0eed12d41d7d87b02f7408
95度茅台 <dj4qiao@sina.com>
2023-11-05 03:04:06 +0800

    update script

--- api/web_main_exchange_rate.js
+++ api/web_main_exchange_rate.js
@@ -851,7 +851,7 @@ async function main() {
         select.addEventListener( 'change', (e) => {
           const selectedValues = Array.from(e.target.selectedOptions, option => option.value);
           formData[item.name] = item.multiple ? selectedValues : selectedValues[0];
-          formData.solidColor = selectedValues.length > 0 ? false : true;
+          formData.solidColor = selectedValues.length > 0 && !item.multiple ? false : true;
           invoke('changeSettings', formData);
         });
         
