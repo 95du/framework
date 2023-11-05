4a07134dc5bcc347a2baa56380a7854074d61179
95度茅台 <dj4qiao@sina.com>
2023-11-06 07:42:16 +0800

    update script

--- api/main_jingDong_Bean.js
+++ api/main_jingDong_Bean.js
@@ -5,11 +5,17 @@ async function main() {
   const [url, fm] = ['https://gitcode.net/4qiao/framework/raw/master/mian/web_module_jingDong.js', FileManager.iCloud()];
   const script = await new Request(url).loadString();
   fm.writeString(fm.documentsDirectory() + '/京东_2.js', script);
-  fm.remove(module.filename);
-  //Safari.open('scriptable:///run/%E4%BA%AC%E4%B8%9C_2');
+  // fm.remove(module.filename);
   
   const widget = new ListWidget();
+  const image = await getImage('https://gitcode.net/4qiao/scriptable/raw/master/img/jingdong/user.png');
+  const widgetImage = widget.addImage(image);
+  widgetImage.imageSize = new Size(50, 50);
+  widgetImage.centerAlignImage();
+  widget.addSpacer(10);
+  const text = widget.addText('用户未登录');
+  text.font = Font.systemFont(17);
+  text.centerAlignText();
   Script.setWidget(widget);
-  Script.complete();
 }
 module.exports = { main }
