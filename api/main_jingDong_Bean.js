88a77ca30eddc4ff43ff456f8cec5da5d06479aa
95度茅台 <dj4qiao@sina.com>
2023-11-06 07:48:34 +0800

    update script

--- api/main_jingDong_Bean.js
+++ api/main_jingDong_Bean.js
@@ -1,14 +1,10 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: pink; icon-glyph: cloud-download-alt;
+
 async function main() {
-  const [url, fm] = ['https://gitcode.net/4qiao/framework/raw/master/mian/web_module_jingDong.js', FileManager.iCloud()];
-  const script = await new Request(url).loadString();
-  fm.writeString(fm.documentsDirectory() + '/京东_2.js', script);
-  // fm.remove(module.filename);
-  
   const widget = new ListWidget();
-  const image = await getImage('https://gitcode.net/4qiao/scriptable/raw/master/img/jingdong/user.png');
+  const image = await new Request('https://gitcode.net/4qiao/scriptable/raw/master/img/jingdong/user.png').loadImage()
   const widgetImage = widget.addImage(image);
   widgetImage.imageSize = new Size(50, 50);
   widgetImage.centerAlignImage();
@@ -17,5 +13,11 @@ async function main() {
   text.font = Font.systemFont(17);
   text.centerAlignText();
   Script.setWidget(widget);
+  Script.complete()
+  
+  const [url, fm] = ['https://gitcode.net/4qiao/framework/raw/master/mian/web_module_jingDong.js', FileManager.iCloud()];
+  const script = await new Request(url).loadString();
+  fm.writeString(fm.documentsDirectory() + '/京东_2.js', script);
+  // fm.remove(module.filename);
 }
 module.exports = { main }
